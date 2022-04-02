7e24ea3d90f72a5d1f8217ccd2d821167ece875c
Casey Tucker <dctucker@hotmail.com>
2022-04-02 15:49:42 -0700

    skip reserved

--- junods/src/ast.nim
+++ junods/src/ast.nim
@@ -1,3 +1,4 @@
+import strutils
 import memorymap
 import macros
 
@@ -104,8 +105,9 @@ proc diveMap(input: NimNode): seq[NimNode] =
         else:
           echo "unexpected kind = " & area.treeRepr
     else:
-      result.add quote do:
-        Mem( offset: `offset`, name: `name`)
+      if not name.contains("reserved"):
+        result.add quote do:
+          Mem( offset: `offset`, name: `name`)
     #echo "name = " & name.repr
   of nnkCall:
     let name = newLit( $input[0].strVal() )
@@ -171,7 +173,7 @@ let vocal_effects: MemArea = @[]
 let patch_drum: MemArea = @[]
 let drum_kits: MemArea = @[]
 
-#let setup = genMap: 0 common: @[]
+let setup = genMap: 0 common: @[]
 let system = genMap:
   0x000000 common:
     0x0000 master:
@@ -183,8 +185,8 @@ let system = genMap:
     0x08 mix_parallel       TBool
     0x09 control_channel    TByte, 0, 16 # 16=OFF
     0x0a kbd_patch_channel  TByte, 0, 15
-    #0x0b, reserved_1        TBool
-    0x0c scale: scale_map
+    0x0b reserved_1        TBool
+    0x0c scale:             scale_map
     control_source:
       0x18 1                TEnum, control_source_values
       0x19 2                TEnum, control_source_values
@@ -199,53 +201,53 @@ let system = genMap:
       0x01 bank             TBool
     0x02 velocity           TByte, 0, 127 # 0=REAL
     0x03 velocity_curve     TEnum, @["LIGHT","MEDIUM","HEAVY"]
-    #0x04 reserved_1
+    0x04 reserved_1
     0x05 hold_polarity      TEnum, polarity
     0x06 continuous_hold    TBool
     control_pedal:
       0x07 assign           TEnum, pedal_assign_values
       0x08 polarity         TEnum, polarity
-    #0x09, reserved_2
+    0x09 reserved_2
     knob_assign:
       0x10 1                TEnum, knob_assign_values
       0x11 2                TEnum, knob_assign_values
       0x12 3                TEnum, knob_assign_values
       0x13 4                TEnum, knob_assign_values
-    #0x14, reserved_2
-    #0x4d, reserved_3
+    0x14 reserved_2
+    0x4d reserved_3
 
-#let juno_map = genMap:
-#  0x01000000 setup: setup
-#  0x02000000 system: system
-#  temporary:
-#    0x10000000 performance_pattern: performance_pattern
-#    performance_part:
-#      0x11000000   1: patch_drum # Temporary Patch/Drum (Performance Mode Part 1)
-#      0x11200000   2: patch_drum
-#      0x11400000   3: patch_drum
-#      0x11600000   4: patch_drum
-#      0x12000000   5: patch_drum
-#      0x12200000   6: patch_drum
-#      0x12400000   7: patch_drum
-#      0x12600000   8: patch_drum
-#      0x13000000   9: patch_drum
-#      0x13200000  10: patch_drum
-#      0x13400000  11: patch_drum
-#      0x13600000  12: patch_drum
-#      0x14000000  13: patch_drum
-#      0x14200000  14: patch_drum
-#      0x14400000  15: patch_drum
-#      0x14600000  16: patch_drum
-#    0x1e000000 rhythm_pattern:      arpeggio
-#    0x1e110000 arpeggio:            arpeggio
-#    0x1e130000 rhythm_group:        rhythm_group
-#    0x1e150000 vocal_effect:        vocal_effect
-#    0x1f000000 patch_part: # patch mode part 1
-#      0x000000 x1: patch_drum
-#      0x200000 x2: patch_drum
-#  user:
-#    0x20000000 performance:  performance_patterns
-#    0x21000000 pattern:      performance_patterns
-#    0x30000000 patch:        performance_patches
-#    0x40000000 drum_kit:     drum_kits
-#    0x60000000 vocal_effect: vocal_effects
+let juno_map = genMap:
+  0x01000000 setup: setup
+  0x02000000 system: system
+  temporary:
+    0x10000000 performance_pattern: performance_pattern
+    performance_part:
+      0x11000000   1: patch_drum # Temporary Patch/Drum (Performance Mode Part 1)
+      0x11200000   2: patch_drum
+      0x11400000   3: patch_drum
+      0x11600000   4: patch_drum
+      0x12000000   5: patch_drum
+      0x12200000   6: patch_drum
+      0x12400000   7: patch_drum
+      0x12600000   8: patch_drum
+      0x13000000   9: patch_drum
+      0x13200000  10: patch_drum
+      0x13400000  11: patch_drum
+      0x13600000  12: patch_drum
+      0x14000000  13: patch_drum
+      0x14200000  14: patch_drum
+      0x14400000  15: patch_drum
+      0x14600000  16: patch_drum
+    0x1e000000 rhythm_pattern:      arpeggio
+    0x1e110000 arpeggio:            arpeggio
+    0x1e130000 rhythm_group:        rhythm_group
+    0x1e150000 vocal_effect:        vocal_effect
+    0x1f000000 patch_part: # patch mode part 1
+      0x000000 x1: patch_drum
+      0x200000 x2: patch_drum
+  user:
+    0x20000000 performance:  performance_patterns
+    0x21000000 pattern:      performance_patterns
+    0x30000000 patch:        performance_patches
+    0x40000000 drum_kit:     drum_kits
+    0x60000000 vocal_effect: vocal_effects
